e739021cb30f4911cf4062676d16118eca1d6b15
Aran Nayebi <anayebi@stanford.edu>
2022-02-28 15:06:29 -0800

    Minimal setup and using tf 2.0.1

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name="mec",
     version="1.0", 
     packages=find_packages(),
+    install_requires=["numpy==1.18.5", 
+                      "scikit-learn==0.23.2", 
+                      "scipy==1.5.2", 
+                      "tensorflow_gpu==2.0.1",
+                      "tqdm==4.49.0",
+                      "xarray==0.15.1"],
     python_requires=">=3.6"
 )
